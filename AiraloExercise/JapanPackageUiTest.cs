ee5648a1b4c9691bf6c9318aa50809c6b421855f
Boris Popov <borisbpopov@gmail.com>
2025-04-02 17:16:54 +0300

    Add Chrome Driver to SetUp and TearDown

--- AiraloExercise/JapanPackageUiTest.cs
+++ AiraloExercise/JapanPackageUiTest.cs
@@ -5,10 +5,15 @@ namespace AiraloExercise
 {
     public class JapanPackageUiTest
     {
+        IWebDriver driver;
 
         [SetUp]
         public void Setup()
         {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--start-maximized"); //Opens the browser in full screen mode
+            options.AddArgument("--disable-notifications"); //Deactivate pending notifications TODO: Check if this is working
+            driver = new ChromeDriver(options);
         }
 
         [Test]
@@ -20,6 +25,7 @@ namespace AiraloExercise
         [TearDown]
         public void TearDown()
         {
+            driver.Quit();
         }
     }
 }
