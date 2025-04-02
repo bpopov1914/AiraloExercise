25645537ee502462d55bc36359359193fa596b29
Boris Popov <borisbpopov@gmail.com>
2025-04-02 17:14:23 +0300

    Add project files.

---
+++ AiraloExercise/JapanPackageUiTest.cs
@@ -0,0 +1,25 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AiraloExercise
+{
+    public class JapanPackageUiTest
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void JapanPackage()
+        {
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+    }
+}
